4ec721780ffcccffc4865d571888c06f62225563
Felipe Lima Coelho <felipeapoloatena@gmail.com>
2023-01-23 18:55:38 -0300

    Criado variável nota e utilizado "if" para atribuir um conceito de A a F dada sua porcentagem

--- 01-modulo-fundamentos/secao-3-introducao-a-javascript/dia-1-javascript-primeiros-passos/exercicios/script.js
+++ 01-modulo-fundamentos/secao-3-introducao-a-javascript/dia-1-javascript-primeiros-passos/exercicios/script.js
@@ -94,3 +94,23 @@ switch (pecaXadrez.toLowerCase()) {
   default:
     console.log("Peça informada não existe no Xadrez");
 }
+
+// Utilize if/else para escrever um código que converta uma nota dada em porcentagem (de 0 a 100) em conceitos de A a F. Siga essas regras:
+
+const nota = 80;
+
+if (nota >= 90 && nota <= 100) {
+  console.log("A");
+} else if (nota >= 80 && nota < 90) {
+  console.log("B");
+} else if (nota >= 70 && nota < 80) {
+  console.log("C");
+} else if (nota >= 60 && nota < 70) {
+  console.log("D");
+} else if (nota >= 50 && nota < 60) {
+  console.log("E");
+} else if (nota > 0 && nota < 50) {
+  console.log("F");
+} else {
+  console.log("ERROR");
+}
