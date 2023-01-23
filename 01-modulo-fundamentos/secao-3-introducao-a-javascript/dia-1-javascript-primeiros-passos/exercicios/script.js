b52c838f125a9482ededec96edbdeb24ca8f2a88
Felipe Lima Coelho <felipeapoloatena@gmail.com>
2023-01-23 15:32:20 -0300

    Utilizado "if/else" para retornar o maior valor das 2 constantes

--- 01-modulo-fundamentos/secao-3-introducao-a-javascript/dia-1-javascript-primeiros-passos/exercicios/script.js
+++ 01-modulo-fundamentos/secao-3-introducao-a-javascript/dia-1-javascript-primeiros-passos/exercicios/script.js
@@ -6,3 +6,9 @@ console.log(a - b);
 console.log(a * b);
 console.log(a / b);
 console.log(a % b);
+
+if (a > b) {
+  console.log(`O maior número entre ${a} e ${b} é o número ${a}`);
+} else {
+  console.log(`O maior número entre ${a} e ${b} é o número ${b}`);
+}
