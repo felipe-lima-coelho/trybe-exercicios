4a65f94de792ec46b7bd0d0ddda577d01e051416
Felipe Lima Coelho <felipeapoloatena@gmail.com>
2023-01-23 19:13:07 -0300

    Utilizando as 3 variáveis number já criadas, verificar se ao menos algum valor é ímpar utilizando um "if"

--- 01-modulo-fundamentos/secao-3-introducao-a-javascript/dia-1-javascript-primeiros-passos/exercicios/script.js
+++ 01-modulo-fundamentos/secao-3-introducao-a-javascript/dia-1-javascript-primeiros-passos/exercicios/script.js
@@ -126,3 +126,11 @@ if (num1 % 2 === 0 || num2 % 2 === 0 || num3 % 3 === 0) {
 } else {
   console.log(false);
 }
+
+// Utilize if/else para escrever um código que defina três números em constantes e retorne true se pelo menos uma das três for ímpar. Caso contrário, ele deve retornar false.
+
+if (num1 % 2 !== 0 || num2 % 2 !== 0 || num3 % 2 !== 0) {
+  console.log(true);
+} else {
+  console.log(false);
+}
