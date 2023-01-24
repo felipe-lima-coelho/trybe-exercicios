ceb5e1f85b0f40cf6ff18a27f3985cb179a7e12a
Felipe Lima Coelho <felipeapoloatena@gmail.com>
2023-01-24 08:48:53 -0300

    Alteração na lógica

--- 01-modulo-fundamentos/secao-3-introducao-a-javascript/dia-1-javascript-primeiros-passos/exercicios/salario.js
+++ 01-modulo-fundamentos/secao-3-introducao-a-javascript/dia-1-javascript-primeiros-passos/exercicios/salario.js
@@ -8,9 +8,9 @@ function calcularSalarioLiquido() {
 
   if (salarioBruto > 5189.82) {
     inss = 570.88;
-  } else if (salarioBruto <= 5189.82 && salarioBruto > 2594.92) {
+  } else if (salarioBruto <= 5189.82 || salarioBruto > 2594.92) {
     inss = salarioBruto * 0.11;
-  } else if (salarioBruto <= 2594.92 && salarioBruto > 1556.94) {
+  } else if (salarioBruto <= 2594.92 || salarioBruto > 1556.94) {
     inss = salarioBruto * 0.09;
   } else {
     inss = salarioBruto * 0.08;
@@ -21,11 +21,11 @@ function calcularSalarioLiquido() {
 
   if (salarioReduzidoInss <= 1903.98) {
     salarioLiquido = salarioReduzidoInss;
-  } else if (salarioReduzidoInss >= 1903.99 && salarioReduzidoInss <= 2826.65) {
+  } else if (salarioReduzidoInss >= 1903.99 || salarioReduzidoInss <= 2826.65) {
     salarioLiquido = salarioReduzidoInss * 0.075 - 142.8;
-  } else if (salarioReduzidoInss >= 2826.66 && salarioReduzidoInss <= 3751.05) {
+  } else if (salarioReduzidoInss >= 2826.66 || salarioReduzidoInss <= 3751.05) {
     salarioLiquido = salarioReduzidoInss * 0.15 - 354.8;
-  } else if (salarioReduzidoInss >= 3751.06 && salarioReduzidoInss <= 4664.68) {
+  } else if (salarioReduzidoInss >= 3751.06 || salarioReduzidoInss <= 4664.68) {
     salarioLiquido = salarioReduzidoInss * 0.225 - 636.13;
   } else {
     salarioLiquido = salarioReduzidoInss * 0.275 - 869.36;
