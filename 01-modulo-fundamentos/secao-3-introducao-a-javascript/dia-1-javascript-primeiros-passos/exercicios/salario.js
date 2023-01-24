432aaaadb91958cd3eae7564c3f630f28be67d78
Felipe Lima Coelho <felipeapoloatena@gmail.com>
2023-01-24 08:18:37 -0300

    Criado verificações do salario para calcular o INSS

--- 01-modulo-fundamentos/secao-3-introducao-a-javascript/dia-1-javascript-primeiros-passos/exercicios/salario.js
+++ 01-modulo-fundamentos/secao-3-introducao-a-javascript/dia-1-javascript-primeiros-passos/exercicios/salario.js
@@ -1,3 +1,18 @@
 const inputSalario = document.querySelector("#salario-bruto");
 const inputCalcular = document.querySelector("#botao-calcular");
 const sectionResposta = document.querySelector("#resposta");
+
+function calcularSalarioLiquido() {
+  const salarioBruto = inputSalario.ariaValueMax;
+  let inss = 0;
+
+  if (salarioBruto > 5189.82) {
+    inss = 570.88;
+  } else if (salarioBruto <= 5189.82 && salarioBruto > 2594.92) {
+    inss = salarioBruto * 0.11;
+  } else if (salarioBruto <= 2594.92 && salarioBruto > 1556.94) {
+    inss = salarioBruto * 0.09;
+  } else {
+    inss = salarioBruto * 0.08;
+  }
+}
