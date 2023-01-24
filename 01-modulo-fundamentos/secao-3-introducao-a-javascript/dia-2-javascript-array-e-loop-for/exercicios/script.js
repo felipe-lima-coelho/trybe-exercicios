9a0cd3b5bdf5a037f00c8a507cdbecf2e82dd676
Felipe Lima Coelho <felipeapoloatena@gmail.com>
2023-01-24 11:29:26 -0300

    Criado uma variável com array vazio e utilizando o for foi adicionado os valores de 1 a 25 nela, por fim foi imprimido o array no console

--- 01-modulo-fundamentos/secao-3-introducao-a-javascript/dia-2-javascript-array-e-loop-for/exercicios/script.js
+++ 01-modulo-fundamentos/secao-3-introducao-a-javascript/dia-2-javascript-array-e-loop-for/exercicios/script.js
@@ -69,3 +69,11 @@ for (let i = 0; i < numbers.length; i += 1) {
 }
 
 console.log(`O menor valor da Array é o ${menoValor}`);
+
+// - Utilizando for, crie um array que vá de 1 até 25 e imprima o resultado
+
+const criarArray = [];
+for (let i = 0; i < 25; i += 1) {
+  criarArray[i] = i + 1;
+}
+console.log(criarArray);
