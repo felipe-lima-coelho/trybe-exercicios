91bb19ac3a3fd4d79fb532ab4678c78cd037246c
Felipe Lima Coelho <felipeapoloatena@gmail.com>
2023-01-24 09:48:12 -0300

    Obtenção do índice do valor “Portfólio” do array menu

--- 01-modulo-fundamentos/secao-3-introducao-a-javascript/dia-2-javascript-array-e-loop-for/arrays/script.js
+++ 01-modulo-fundamentos/secao-3-introducao-a-javascript/dia-2-javascript-array-e-loop-for/arrays/script.js
@@ -1,6 +1,13 @@
-// Obtenha o valor “Serviços” do array menu
+// - Obtenha o valor “Serviços” do array menu
+
+// let menu = ["Home", "Serviços", "Portfólio", "Links"];
+// let menuServices = menu[1];
+
+// console.log(menuServices);
+
+// - Procure o índice do valor “Portfólio” do array menu
 
 let menu = ["Home", "Serviços", "Portfólio", "Links"];
-let menuServices = menu[1];
+let indexOfPortfolio = menu.indexOf("Portfólio");
 
-console.log(menuServices);
+console.log(indexOfPortfolio);
