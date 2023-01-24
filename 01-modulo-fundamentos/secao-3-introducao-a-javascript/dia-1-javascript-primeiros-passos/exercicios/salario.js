d34e166cda67036fa0e18c552d0b8ed67743a925
Felipe Lima Coelho <felipeapoloatena@gmail.com>
2023-01-24 08:06:26 -0300

    Selecionado os inputs e a seção de resposta no js

---
+++ 01-modulo-fundamentos/secao-3-introducao-a-javascript/dia-1-javascript-primeiros-passos/exercicios/salario.js
@@ -0,0 +1,3 @@
+const inputSalario = document.querySelector("#salario-bruto");
+const inputCalcular = document.querySelector("#botao-calcular");
+const sectionResposta = document.querySelector("#resposta");
