77ec72ce2e2fb3886de6cc0a0ba51d4eef2a3f26
Felipe Lima Coelho <felipeapoloatena@gmail.com>
2023-01-25 15:09:41 -0300

    Utilizado for para inverter as strings "tryber" e "brasil"

--- 01-modulo-fundamentos/secao-3-introducao-a-javascript/dia-3-javaScript-logica-de-programacaoo-e-algoritmos/exercicios.js
+++ 01-modulo-fundamentos/secao-3-introducao-a-javascript/dia-3-javaScript-logica-de-programacaoo-e-algoritmos/exercicios.js
@@ -11,3 +11,23 @@ for (let i = 9; i >= 0; i -= 1) {
 console.log(
   `${aFat[9]}! = ${aFat[9]} x ${aFat[8]} x ${aFat[7]} x ${aFat[6]} x ${aFat[5]} x ${aFat[4]} x ${aFat[3]} x ${aFat[2]} x ${aFat[1]} x ${aFat[0]} = ${multip}`
 );
+
+// - Utilize a estrutura de repetição for para desenvolver um algoritmo que seja capaz de inverter uma palavra. Por exemplo, a palavra “banana” seria invertida para “ananab”. Utilize a string abaixo como exemplo, depois troque por outras para verificar se seu algoritmo está funcionando corretamente.
+
+let word = "tryber";
+let newWord = "";
+
+for (let i = word.length - 1; i >= 0; i -= 1) {
+  newWord += word[i];
+}
+
+console.log(`Palavra invertida: ${newWord}`);
+
+let secWord = "brasil";
+newWord = "";
+
+for (let i = secWord.length - 1; i >= 0; i -= 1) {
+  newWord += secWord[i];
+}
+
+console.log(`Nova palavra invertida: ${newWord}`);
