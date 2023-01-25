9b9c1351b34feeb9db75b456d48f7ab643a6c8de
Felipe Lima Coelho <felipeapoloatena@gmail.com>
2023-01-25 19:01:34 -0300

    Criado um algorítimo que imprime um triângulo retângulo de asteriscos utilizando apenas um for

--- 01-modulo-fundamentos/secao-3-introducao-a-javascript/dia-3-javaScript-logica-de-programacaoo-e-algoritmos/bonus.js
+++ 01-modulo-fundamentos/secao-3-introducao-a-javascript/dia-3-javaScript-logica-de-programacaoo-e-algoritmos/bonus.js
@@ -8,7 +8,7 @@
 // *****
 // *****
 
-let n = 6;
+let n = 5;
 let asterisco = "";
 
 for (let ia = 1; ia <= n; ia += 1) {
@@ -17,4 +17,19 @@ for (let ia = 1; ia <= n; ia += 1) {
   }
   console.log(asterisco);
   asterisco = "";
+}
+
+// Faça o mesmo que antes, mas que imprima um triângulo retângulo com 5 asteriscos de base. Por exemplo:
+
+// n = 5
+
+// *
+// **
+// ***
+// ****
+// *****
+
+for (let ia = 1; ia <= n; ia += 1) {
+  asterisco += "*";
+  console.log(asterisco);
 }
