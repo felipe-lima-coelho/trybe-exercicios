8c39fe97fe729384102342931be4624369297415
Felipe Lima Coelho <felipeapoloatena@gmail.com>
2023-01-26 12:49:03 -0300

    Criado função para verificar se o tamanho dos angulos de um triangulo, passados pelos parâmetros, estão corretos para serem triângulos

--- 01-modulo-fundamentos/secao-3-introducao-a-javascript/dia-4-javascript-objetos-e-funcoes/funcoes/script.js
+++ 01-modulo-fundamentos/secao-3-introducao-a-javascript/dia-4-javascript-objetos-e-funcoes/funcoes/script.js
@@ -64,3 +64,19 @@ function verificarNumero(a) {
     return "zero";
   }
 }
+
+// - Faça um programa que receba três constantes com os valores dos três ângulos internos de um triângulo. Retorne true se os ângulos representarem os ângulos de um triângulo e false, caso contrário. Se algum ângulo for inválido, o programa deve retornar uma mensagem de erro.
+
+function verificaTriangulo(angA, angB, angC) {
+  const somaAngulo = angA + angB + angC;
+
+  if (angA > 0 || angB > 0 || angC > 0) {
+    if (somaAngulo === 180) {
+      return true;
+    } else {
+      return false;
+    }
+  } else {
+    return "ERROR: algum ângulo é negativo ou igual a zero";
+  }
+}
