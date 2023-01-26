007dcbbbcbcd63edf508ee9de12d21ae12d0b770
Felipe Lima Coelho <felipeapoloatena@gmail.com>
2023-01-26 15:40:26 -0300

    Exibido no console os valores das propriedades do objeto "info" utilizando o for-in

--- 01-modulo-fundamentos/secao-3-introducao-a-javascript/dia-4-javascript-objetos-e-funcoes/exercicios-objetos-e-for-in/script.js
+++ 01-modulo-fundamentos/secao-3-introducao-a-javascript/dia-4-javascript-objetos-e-funcoes/exercicios-objetos-e-for-in/script.js
@@ -33,3 +33,14 @@ console.log(info);
 for (let i in info) {
   console.log(i);
 }
+
+// - Faça um novo for/in, mas agora mostre todos os valores das chaves do objeto. Valor esperado no console:
+
+// Margarida
+// Pato Donald
+// Namorada do personagem principal nos quadrinhos do Pato Donald
+// Sim
+
+for (let i in info) {
+  console.log(info[i]);
+}
