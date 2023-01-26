3d183c8cdf0b8fd68bb8ff87667e0fb824c34795
Felipe Lima Coelho <felipeapoloatena@gmail.com>
2023-01-26 11:56:26 -0300

    Utilizando o for/in foi impresso as chaves e seus valores de um objeto car

--- 01-modulo-fundamentos/secao-3-introducao-a-javascript/dia-4-javascript-objetos-e-funcoes/for-in-e-for-of/script.js
+++ 01-modulo-fundamentos/secao-3-introducao-a-javascript/dia-4-javascript-objetos-e-funcoes/for-in-e-for-of/script.js
@@ -9,3 +9,15 @@ let names = {
 for (let index in names) {
   console.log(`Olá ${names[index]}`);
 }
+
+// - Usando o objeto abaixo, utilize for/in e imprima um console.log com as chaves e valores desse objeto.
+
+let car = {
+  model: "A3 Sedan",
+  manufacturer: "Audi",
+  year: 2020,
+};
+
+for (let index in car) {
+  console.log(index, car[index]);
+}
