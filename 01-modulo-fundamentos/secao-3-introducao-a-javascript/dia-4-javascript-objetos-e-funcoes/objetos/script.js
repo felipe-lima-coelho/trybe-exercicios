0ff4f121aea02d53f3461f714bc318bf3fa40672
Felipe Lima Coelho <felipeapoloatena@gmail.com>
2023-01-26 11:07:49 -0300

    Criado um objeto a partir de algumas variáveis propostas

---
+++ 01-modulo-fundamentos/secao-3-introducao-a-javascript/dia-4-javascript-objetos-e-funcoes/objetos/script.js
@@ -0,0 +1,16 @@
+// - Crie uma variável player e atribua um objeto contendo as variáveis listadas abaixo:
+
+// let name = 'Marta';
+// let lastName = 'Silva';
+// let age = 34;
+// let medals = { golden: 2, silver: 3 };
+
+const player = {
+  name: "Marta",
+  lastName: "Silva",
+  age: 34,
+  medals: {
+    golden: 2,
+    silver: 3,
+  },
+};
