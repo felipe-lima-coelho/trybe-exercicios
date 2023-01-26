ac9ad82de4b1b9a6e4db09e6c884aa1551d51b4b
Felipe Lima Coelho <felipeapoloatena@gmail.com>
2023-01-26 16:15:57 -0300

    Impresso no console uma frase contendo os valores de nome e aquantidade de livros favoritos do objeto leitor

--- 01-modulo-fundamentos/secao-3-introducao-a-javascript/dia-4-javascript-objetos-e-funcoes/exercicios-objetos-e-for-in/script.js
+++ 01-modulo-fundamentos/secao-3-introducao-a-javascript/dia-4-javascript-objetos-e-funcoes/exercicios-objetos-e-for-in/script.js
@@ -99,3 +99,9 @@ leitor.livrosFavoritos[1] = {
   autor: "JK Rowling",
   editora: "Rocco",
 };
+
+// - Acesse as chaves nome e livrosFavoritos e faça um console.log no seguinte formato: “Julia tem <quantidade> livros favoritos”, onde “<quantidade>” é um número gerado automaticamente pelo seu código.
+
+console.log(
+  `${leitor.nome} tem ${leitor.livrosFavoritos.length} livros favoritos`
+);
