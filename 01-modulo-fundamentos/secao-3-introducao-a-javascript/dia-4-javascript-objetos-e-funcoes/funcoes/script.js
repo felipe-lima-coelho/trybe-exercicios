417c4bc8bf5d9b60b5c8199359e7ecc1243d058b
Felipe Lima Coelho <felipeapoloatena@gmail.com>
2023-01-26 12:41:25 -0300

    Criado uma função para verificar se o parâmetro é positivo, negativo ou zero

--- 01-modulo-fundamentos/secao-3-introducao-a-javascript/dia-4-javascript-objetos-e-funcoes/funcoes/script.js
+++ 01-modulo-fundamentos/secao-3-introducao-a-javascript/dia-4-javascript-objetos-e-funcoes/funcoes/script.js
@@ -52,3 +52,15 @@ function maiorValor3(a, b, c) {
     return c;
   }
 }
+
+// - Faça um programa que, dado um valor recebido como parâmetro, retorne “positive” se esse valor for positivo, “negative” se for negativo, e caso não seja nem positivo e nem negativo retorne “zero”.
+
+function verificarNumero(a) {
+  if (a > 0) {
+    return "positive";
+  } else if (a < 0) {
+    return "negative";
+  } else {
+    return "zero";
+  }
+}
