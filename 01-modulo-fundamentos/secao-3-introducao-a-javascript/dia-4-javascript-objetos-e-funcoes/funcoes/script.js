3e07e10fce3b611dac2f0eefd8f4eef258f8c84c
Felipe Lima Coelho <felipeapoloatena@gmail.com>
2023-01-26 12:10:23 -0300

    Criado duas constantes com valor number e 5 funções de operações recebendo os valores como parametro

---
+++ 01-modulo-fundamentos/secao-3-introducao-a-javascript/dia-4-javascript-objetos-e-funcoes/funcoes/script.js
@@ -0,0 +1,30 @@
+// - Faça cinco programas, um para cada operação aritmética básica. Seu programa deve ter duas constantes, a e b, definidas no começo com os valores que serão operados. Faça programas para:
+
+// Adição (a + b)
+// Subtração (a - b)
+// Multiplicação (a * b)
+// Divisão (a / b)
+// Módulo (a % b)
+
+const a = 10;
+const b = 2;
+
+function adicao(a, b) {
+  return a + b;
+}
+
+function subtracao(a, b) {
+  return a - b;
+}
+
+function multiplicacao(a, b) {
+  return a * b;
+}
+
+function divisao(a, b) {
+  return a / b;
+}
+
+function modulo(a, b) {
+  return a % b;
+}
