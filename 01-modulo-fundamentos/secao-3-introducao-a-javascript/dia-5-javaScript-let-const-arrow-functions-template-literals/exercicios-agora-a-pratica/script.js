7dff7eaaa3c0c819f6a1f32caeb89d519fcb2911
Felipe Lima Coelho <felipeapoloatena@gmail.com>
2023-01-29 14:00:18 -0300

    Alterado var para let na declaração da variável "idade"

---
+++ 01-modulo-fundamentos/secao-3-introducao-a-javascript/dia-5-javaScript-let-const-arrow-functions-template-literals/exercicios-agora-a-pratica/script.js
@@ -0,0 +1,17 @@
+// - Modifique a variável para que respeite o escopo onde está sendo declarada. Lembre-se: para que a variável respeite o escopo, ela não deve ser acessível fora do escopo em que esteja sendo declarada.
+// - Copie o código abaixo e rode-o para verificar sua saída:
+
+// function imprimeIdade() {
+//   for (var idade = 30; idade <= 40; idade += 1) {
+//     console.log('Idade dentro do for:', idade)
+//   }
+//   console.log('Idade fora do for:', idade) // retire essa linha ao implementar o tipo de variável correta.
+// }
+// imprimeIdade()
+
+function imprimeIdade() {
+  for (let idade = 30; idade <= 40; idade += 1) {
+    console.log('Idade dentro do for:', idade)
+  }
+}
+imprimeIdade()
