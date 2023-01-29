be9180ba34c98e6e26f9719af3d65c12f21d7ac6
Felipe Lima Coelho <felipeapoloatena@gmail.com>
2023-01-29 14:06:39 -0300

    Alterado const para let na variável "favoriteFood"

--- 01-modulo-fundamentos/secao-3-introducao-a-javascript/dia-5-javaScript-let-const-arrow-functions-template-literals/exercicios-agora-a-pratica/script.js
+++ 01-modulo-fundamentos/secao-3-introducao-a-javascript/dia-5-javaScript-let-const-arrow-functions-template-literals/exercicios-agora-a-pratica/script.js
@@ -42,3 +42,14 @@ pessoa = {
 } // Altere essa estrutura para corrigir o erro.
 console.log('Nome:', pessoa.nome);
 console.log('Idade:', pessoa.idade);
+
+// - Modifique a variável para que não ocorra Erro;
+// - Copie o código abaixo e rode-o para verificar sua saída:
+
+// const favoriteFood = 'Lasanha';
+// favoriteFood = 'Hamburguer';
+// console.log(favoriteFood);
+
+let favoriteFood = 'Lasanha';
+favoriteFood = 'Hamburguer';
+console.log(favoriteFood);
