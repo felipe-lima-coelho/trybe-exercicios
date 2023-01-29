36f6c2e50623fb16b47edf59e80eb2175bbc22ab
Felipe Lima Coelho <felipeapoloatena@gmail.com>
2023-01-29 18:39:23 -0300

    Criado uma função que recebe o retorno da Função 1 por parâmetro e retorna uma nova string concatenada com a função 1 e 3 tecnologias de um array

--- 01-modulo-fundamentos/secao-3-introducao-a-javascript/dia-5-javaScript-let-const-arrow-functions-template-literals/exercicios-bonus/script.js
+++ 01-modulo-fundamentos/secao-3-introducao-a-javascript/dia-5-javaScript-let-const-arrow-functions-template-literals/exercicios-bonus/script.js
@@ -14,3 +14,20 @@ const substituaX = (nome) => {
 };
 
 console.log(substituaX('Felipe'));
+
+// - Função 2: Escreva uma função que irá receber o retorno da Função 1 por parâmetro e retornará uma nova string.
+//    . O nome da função deverá ser minhasSkills;
+//    . A função deverá receber o retorno da Função 1 - substituaX por parâmetro;
+//    . Declare dentro da função uma variável com o nome skills, do tipo const;
+//        . A variável skills deverá ser um array contendo três strings com tecnologias que você já aprendeu.
+//    . Crie uma varável do tipo let e concatene o valor retornado da Função 1 - substituaX (utilize template literals), a frase 'Minhas três principais habilidades são:' e o valor da variável skills.
+
+const minhasSkills = (substituaX) => {
+  const skills = ['HTML', 'CSS', 'JavaScript'];
+  let fraseConcatenada = `${substituaX}
+Minhas três principais habilidades são: ${skills[0]}, ${skills[1]}, ${skills[2]}.`
+  
+  return fraseConcatenada;
+};
+
+console.log(minhasSkills(substituaX('Felipe')));
