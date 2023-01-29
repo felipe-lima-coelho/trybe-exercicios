fab075db42e98ce0ab0fb1133051bc5a0f2c94bb
Felipe Lima Coelho <felipeapoloatena@gmail.com>
2023-01-28 22:26:16 -0300

    Impresso no console os dados do último morador contido em um objeto "moradores"

--- 01-modulo-fundamentos/secao-3-introducao-a-javascript/dia-4-javascript-objetos-e-funcoes/exercicios-bonus/script.js
+++ 01-modulo-fundamentos/secao-3-introducao-a-javascript/dia-4-javascript-objetos-e-funcoes/exercicios-bonus/script.js
@@ -105,3 +105,42 @@ for (let i in numFrutas(basket, arrayFrutas)) {
 fraseConsole = fraseConsole.slice(0, -1) + '.';
 
 console.log(fraseConsole);
+
+// - Usando o objeto abaixo, faça o que for pedido a seguir:
+
+let moradores = {
+  blocoUm: [
+    {
+      nome: 'Luiza',
+      sobrenome: 'Guimarães',
+      andar: 10,
+      apartamento: 1005,
+    },
+    {
+      nome: 'William',
+      sobrenome: 'Albuquerque',
+      andar: 5,
+      apartamento: 502,
+    },
+  ],
+  blocoDois: [
+    {
+      nome: 'Murilo',
+      sobrenome: 'Ferraz',
+      andar: 8,
+      apartamento: 804,
+    },
+    {
+      nome: 'Zoey',
+      sobrenome: 'Brooks',
+      andar: 1,
+      apartamento: 101,
+    },
+  ],
+};
+
+// - Acesse as chaves nome, sobrenome, andar e apartamento do último morador do blocoDois e faça um console.log no seguinte formato: “O morador do bloco 2 de nome Zoey Brooks mora no 1° andar, apartamento 101”.
+
+const ultimoMorador = moradores.blocoDois[moradores.blocoDois.length - 1];
+
+console.log(`O morador do bloco 2 de nome ${ultimoMorador.nome} ${ultimoMorador.sobrenome} mora no ${ultimoMorador.andar}º andar, apartamento ${ultimoMorador.apartamento}`);
