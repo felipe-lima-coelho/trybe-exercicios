8f64fd914c0d4ea0cece1519a3ee13129d0c9352
Felipe Lima Coelho <felipeapoloatena@gmail.com>
2023-02-02 09:11:01 -0300

    Criado um evento de click onde mantem apenas uma classe tech na lista

--- 01-modulo-fundamentos/secao-4-javaScript-dom-eventos-web-storage/dia-3-javascript-eventos/exercicio-conheca-addeventlistener/main.js
+++ 01-modulo-fundamentos/secao-4-javaScript-dom-eventos-web-storage/dia-3-javascript-eventos/exercicio-conheca-addeventlistener/main.js
@@ -12,6 +12,21 @@ const myWebpage = document.getElementById('my-spotrybefy');
 // - Crie uma função que adicione a classe 'tech' ao elemento `li` quando for clicado.
 //  - Deve existir apenas um elemento com a classe 'tech'. Como você faz isso?
 
+const containerLista = document.querySelector(".container");
+const lista = document.querySelectorAll(".container li");
+
+const removeTech = () => {
+  lista.forEach((item) => {
+    item.classList.remove("tech");
+  });
+};
+
+containerLista.addEventListener("click", (event) => {
+  removeTech();
+
+  event.target.classList.add("tech");
+});
+
 // - Crie uma função que, ao digitar na caixa de texto, altere o texto do elemento
 // com a classe 'tech';
 
