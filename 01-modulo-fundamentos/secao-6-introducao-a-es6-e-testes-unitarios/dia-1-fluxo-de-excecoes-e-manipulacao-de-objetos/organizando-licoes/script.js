85f418e7085f1208da23cd5a7570c4760e2eb34f
Felipe Lima Coelho <felipeapoloatena@gmail.com>
2023-02-18 19:48:43 -0300

    Criado um objeto de nomo allLessons que agrupa todas as aulas

--- 01-modulo-fundamentos/secao-6-introducao-a-es6-e-testes-unitarios/dia-1-fluxo-de-excecoes-e-manipulacao-de-objetos/organizando-licoes/script.js
+++ 01-modulo-fundamentos/secao-6-introducao-a-es6-e-testes-unitarios/dia-1-fluxo-de-excecoes-e-manipulacao-de-objetos/organizando-licoes/script.js
@@ -51,3 +51,33 @@ const viewValuesObj = (obj) => {
 };
 
 console.log(viewValuesObj(lesson2));
+
+// Crie um objeto de nome allLessons, que deve agrupar todas as aulas através do Object.assign. Cada chave desse novo objeto será uma aula, sendo essas chaves lesson1, lesson2 e lesson3. Ao executar o comando console.log(allLessons), a saída deverá ser a seguinte:
+
+// console.log(allLessons);
+/*
+{
+  lesson1:
+   { materia: 'Matemática',
+     numeroEstudantes: 20,
+     professor: 'Maria Clara',
+     turno: 'manhã' },
+  lesson2:
+   { materia: 'História',
+     numeroEstudantes: 20,
+     professor: 'Carlos',
+     turno: 'noite' },
+  lesson3:
+   { materia: 'Matemática',
+     numeroEstudantes: 10,
+     professor: 'Maria Clara',
+     turno: 'noite' }
+};
+*/
+
+const allLessons = {};
+allLessons.lesson1 = Object.assign({}, lesson1);
+allLessons.lesson2 = Object.assign({}, lesson2);
+allLessons.lesson3 = Object.assign({}, lesson3);
+
+console.log(allLessons);
