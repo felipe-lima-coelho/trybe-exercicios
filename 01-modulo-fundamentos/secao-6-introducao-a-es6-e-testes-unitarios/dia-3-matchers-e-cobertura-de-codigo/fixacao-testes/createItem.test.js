3dfb7861bb365912742e2a71b56a2f9c4ce0e6e7
Felipe Lima Coelho <felipeapoloatena@gmail.com>
2023-02-26 22:03:18 -0300

    Criado um teste para verificar se um item é valido quando utilizado a função "createItem"

--- 01-modulo-fundamentos/secao-6-introducao-a-es6-e-testes-unitarios/dia-3-matchers-e-cobertura-de-codigo/fixacao-testes/createItem.test.js
+++ 01-modulo-fundamentos/secao-6-introducao-a-es6-e-testes-unitarios/dia-3-matchers-e-cobertura-de-codigo/fixacao-testes/createItem.test.js
@@ -1,7 +1,14 @@
 const createItem = require('./createItem');
 
 describe('a função createItem', () => {
-  it.todo('cria um item válido');
+  it('cria um item válido', () => {
+    expect(createItem('Felipe', 1, 10, 2)).toEqual({
+      name: 'Felipe',
+      quantity: 2,
+      unit: 1,
+      price: 10,
+    });
+  });
   it.todo('utiliza zero como quantidade padrão');
   it.todo('Lança um erro quando não recebe parâmetros');
   it.todo('Lança um erro se o nome do item não é uma string');
