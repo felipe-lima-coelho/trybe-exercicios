db52bbf3ba47b620ef5ec5cd579ef35f835816c9
Felipe Lima Coelho <felipeapoloatena@gmail.com>
2023-02-26 22:08:50 -0300

    Criado um teste para verificar se o zero é a quantidade padrão quando utilizado a função "createItem"

--- 01-modulo-fundamentos/secao-6-introducao-a-es6-e-testes-unitarios/dia-3-matchers-e-cobertura-de-codigo/fixacao-testes/createItem.test.js
+++ 01-modulo-fundamentos/secao-6-introducao-a-es6-e-testes-unitarios/dia-3-matchers-e-cobertura-de-codigo/fixacao-testes/createItem.test.js
@@ -2,14 +2,21 @@ const createItem = require('./createItem');
 
 describe('a função createItem', () => {
   it('cria um item válido', () => {
-    expect(createItem('Felipe', 1, 10, 2)).toEqual({
-      name: 'Felipe',
-      quantity: 2,
-      unit: 1,
-      price: 10,
+    expect(createItem('banana', 'kg', 1.99, 20)).toEqual({
+      name: 'banana',
+      quantity: 20,
+      unit: 'kg',
+      price: 1.99,
     });
   });
-  it.todo('utiliza zero como quantidade padrão');
+  it('utiliza zero como quantidade padrão', () => {
+    expect(createItem('banana', 'kg', 1.99)).toEqual({
+      name: 'banana',
+      quantity: 0,
+      unit: 'kg',
+      price: 1.99,
+    });
+  });
   it.todo('Lança um erro quando não recebe parâmetros');
   it.todo('Lança um erro se o nome do item não é uma string');
   it.todo('Lança um erro se o preço é negativo');
