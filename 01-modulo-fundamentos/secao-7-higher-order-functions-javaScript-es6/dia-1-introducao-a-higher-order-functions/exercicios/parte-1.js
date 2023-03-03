51167976ab1763e18048cebaa2bc972afdc8fb7e
Felipe Lima Coelho <felipeapoloatena@gmail.com>
2023-03-03 03:59:26 -0300

    Criado uma função que sorteia um número de loteria e retorna com o resultado

--- 01-modulo-fundamentos/secao-7-higher-order-functions-javaScript-es6/dia-1-introducao-a-higher-order-functions/exercicios/parte-1.js
+++ 01-modulo-fundamentos/secao-7-higher-order-functions-javaScript-es6/dia-1-introducao-a-higher-order-functions/exercicios/parte-1.js
@@ -19,3 +19,25 @@ const newEmployees = (callback) => {
 };
 
 console.log(newEmployees(emailName));
+
+// // Sorteador de loteria
+
+// Desenvolva uma HOF que retorne o resultado de um sorteio. Essa HOF irá gerar um número aleatório entre 1 e 5, recebendo como parâmetros o número apostado e uma função que checa se o número apostado é igual ao número sorteado. O retorno da sua HOF deve ser uma string (Ex: “Tente novamente” ou “Parabéns, você ganhou”).
+
+const checkNumber = (number) => {
+  const randomNumbers = Math.floor(Math.random() * 6);
+  
+  if (number !== randomNumbers) {
+    return false;
+  }
+  return true;
+}
+
+const prizeDraw = (number, callback) => {
+  if (callback(number)) {
+    return 'Parabéns, você ganhou';
+  }
+  return 'Tente novamente';
+};
+
+console.log(prizeDraw(5, checkNumber));
