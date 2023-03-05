ac0ef562496b81f311da4c49f6d53568835bc10b
Felipe Lima Coelho <felipeapoloatena@gmail.com>
2023-03-05 17:41:05 -0300

    Criado uma função para retornar o dano aleatório do dragão

---
+++ 01-modulo-fundamentos/secao-7-higher-order-functions-javaScript-es6/dia-1-introducao-a-higher-order-functions/exercicios-bonus/script.js
@@ -0,0 +1,39 @@
+// // Parte I
+
+const mage = {
+  healthPoints: 130,
+  intelligence: 45,
+  mana: 125,
+  damage: undefined,
+};
+
+const warrior = {
+  healthPoints: 200,
+  strength: 30,
+  weaponDmg: 2,
+  damage: undefined,
+};
+
+const dragon = {
+  healthPoints: 350,
+  strength: 50,
+  damage: undefined,
+};
+
+const battleMembers = { mage, warrior, dragon };
+
+// Crie uma função que retorne o dano do dragão
+
+const dragonDamage = () => dragon.damage;
+
+// O dano será um número aleatório entre 15 (dano mínimo) e o valor do atributo strength (dano máximo).
+
+const randonNumber = (min, max) => {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1) + min);
+};
+
+dragon.damage = randonNumber(15, dragon.strength);
+
+console.log(dragonDamage());
