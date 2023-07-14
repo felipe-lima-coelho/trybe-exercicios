68d34001b61a285bb75e53c7b24d090b392fb952
Felipe Lima Coelho <felipeapoloatena@gmail.com>
2023-07-14 18:51:00 -0300

    Exercício 3: concluído

--- 03-modulo-back-end/secao-4-introdução-ao-desenvolvimento-web-com-nodejs/dia-1-nodejs-runtime-assincrono/exercicio/bmi.js
+++ 03-modulo-back-end/secao-4-introdução-ao-desenvolvimento-web-com-nodejs/dia-1-nodejs-runtime-assincrono/exercicio/bmi.js
@@ -1,7 +1,9 @@
-const weight = 100;
-const height = 1.87;
+const readline = require('readline-sync');
 
 const bmi = () => {
+  const weight = readline.questionInt('What’s your weight? ');
+  const height = readline.questionFloat('What’s your height? ');
+
   const calculate = weight / (height ** 2);
   console.log(calculate);
   return calculate;
