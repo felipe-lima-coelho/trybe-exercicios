5a4bcc75f8b4ddad2b887b67c4ff412711a88bfd
Felipe Lima Coelho <felipeapoloatena@gmail.com>
2023-07-14 19:00:02 -0300

    Exercício 5: concluído

--- 03-modulo-back-end/secao-4-introdução-ao-desenvolvimento-web-com-nodejs/dia-1-nodejs-runtime-assincrono/exercicio/bmi.js
+++ 03-modulo-back-end/secao-4-introdução-ao-desenvolvimento-web-com-nodejs/dia-1-nodejs-runtime-assincrono/exercicio/bmi.js
@@ -3,8 +3,23 @@ const readline = require('readline-sync');
 const bmi = () => {
   const weight = readline.questionFloat('What’s your weight? ');
   const height = readline.questionFloat('What’s your height in meters? ');
-
   const calculate = weight / (height ** 2);
+
+  if (calculate < 18.5) {
+    console.log('Abaixo do peso (magreza)');
+  } else if (calculate >= 18.5 && calculate <= 24.9) {
+    console.log('Peso normal');
+  } else if (calculate >= 25 && calculate <= 29.9) {
+    console.log('Acima do peso (sobrepeso)');
+  } else if (calculate >= 30 && calculate <= 34.9) {
+    console.log('Obesidade grau I');
+  } else if (calculate >= 35 && calculate <= 39.9) {
+    console.log('Obesidade grau II');
+  } else {
+    console.log('Obesidade graus III e IV');
+  }
+
+
   console.log(calculate);
   return calculate;
 };
