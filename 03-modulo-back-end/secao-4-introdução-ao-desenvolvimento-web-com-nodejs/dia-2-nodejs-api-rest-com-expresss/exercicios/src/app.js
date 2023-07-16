72a931b6030ef4dbbbac594d0390596a1ea2f23e
Felipe Lima Coelho <felipeapoloatena@gmail.com>
2023-07-16 11:05:04 -0300

    Exercício 5: concluído

--- 03-modulo-back-end/secao-4-introdução-ao-desenvolvimento-web-com-nodejs/dia-2-nodejs-api-rest-com-expresss/exercicios/src/app.js
+++ 03-modulo-back-end/secao-4-introdução-ao-desenvolvimento-web-com-nodejs/dia-2-nodejs-api-rest-com-expresss/exercicios/src/app.js
@@ -1,5 +1,21 @@
 const express = require('express');
+const readFileJson = require('./utils/readFileJson');
 
 const app = express();
+
+app.use(express.json());
+
+app.get('/movies/:id', async (req, res) => {
+  const { id } = req.params;
+  const readFile = await readFileJson();
+
+  const findMovie = readFile.find((movie) => movie.id === Number(id));
+
+  if (!findMovie) {
+    return res.status(404).json({ message: 'Movie not found' });
+  }
+
+  res.status(200).json(findMovie);
+});
 
 module.exports = app;
