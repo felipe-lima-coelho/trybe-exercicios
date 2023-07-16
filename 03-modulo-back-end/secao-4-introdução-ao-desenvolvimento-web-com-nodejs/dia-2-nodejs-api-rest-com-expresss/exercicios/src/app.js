1acbb08541f6f2423eec5936802d63a1daf7490d
Felipe Lima Coelho <felipeapoloatena@gmail.com>
2023-07-16 11:08:29 -0300

    Exercício 6: concluído

--- 03-modulo-back-end/secao-4-introdução-ao-desenvolvimento-web-com-nodejs/dia-2-nodejs-api-rest-com-expresss/exercicios/src/app.js
+++ 03-modulo-back-end/secao-4-introdução-ao-desenvolvimento-web-com-nodejs/dia-2-nodejs-api-rest-com-expresss/exercicios/src/app.js
@@ -5,6 +5,12 @@ const app = express();
 
 app.use(express.json());
 
+app.get('/movies', async (req, res) => {
+  const readFile = await readFileJson();
+
+  res.status(200).json({ movies: readFile });
+});
+
 app.get('/movies/:id', async (req, res) => {
   const { id } = req.params;
   const readFile = await readFileJson();
